2d71ec7913d4675f536c5779ab547b59ed619d35
Atsushi Tabata <a.tabata@bizreach.co.jp>
2026-03-18 12:58:08 +0900

    st08のtest_java8_lambda_callback_order()まで

--- src/test/java/org/docksidestage/javatry/basic/Step08Java8FunctionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step08Java8FunctionTest.java
@@ -86,7 +86,7 @@ public class Step08Java8FunctionTest extends PlainTestCase {
             log(stage);
         });
         log("lost river");
-        // your answer? => 
+        // your answer? => harbor,broadway,dockside,hangar,lost river
     }
 
     private class St8BasicConsumer implements Consumer<String> {
